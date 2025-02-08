f05a6bb61f158bf0ad13c4c924696c94f2d9d796
http418 <http418@163.com>
2025-02-08 21:16:05 +0800

    main

--- v2/in/point.go
+++ v2/in/point.go
@@ -4,6 +4,6 @@ type Point struct {
     Holder   string `json:"holder,omitempty"`
     Item     string `json:"item,omitempty"`
     Offer    uint8  `json:"offer,omitempty"`
-    PriceKey string `json:"price_key,omitempty"`
+    PriceKey int    `json:"price_key,omitempty"`
     Term     int    `json:"term,omitempty"`
 }
